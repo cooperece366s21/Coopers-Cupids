b53b1d7b200865a11be28d888c09d4ea1fd06d76
Mark Koszykowski <markkoszykowski@gmail.com>
2021-03-10 18:46:12 -0500

    prototype

--- src/main/java/edu/cooper/ece366/App.java
+++ src/main/java/edu/cooper/ece366/App.java
@@ -15,10 +15,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import spark.ResponseTransformer;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
