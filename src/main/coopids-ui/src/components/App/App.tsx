e6386196e7873b325736884b023fc2b00f330e39
Andrew Lorber <andrewlorber@aol.com>
2021-04-14 16:58:59 -0400

    Updates app component

--- src/main/coopids-ui/src/components/App/App.tsx
+++ src/main/coopids-ui/src/components/App/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from '../../logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Sets types
+type AppProps = {};
+type AppState = {};
+
+class App extends Component<AppProps, AppState> {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  async componentDidMount() {
+    // Make backend requests here
+    this.setState({});
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+    );
+  }
 }
 
 export default App;
