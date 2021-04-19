abc6a24808296d7ad41cb25be262e14a6738ef92
Andrew Lorber <andrewlorber@aol.com>
2021-04-19 13:26:03 -0400

    Adds conversation menu

--- src/main/coopids-ui/src/components/layouts/MessagesLayout/MessageLayout.tsx
+++ src/main/coopids-ui/src/components/layouts/MessagesLayout/MessageLayout.tsx
@@ -1,12 +1,41 @@
 import React, {Component} from "react";
+import ConversationMenu from "../../ui/ConversationMenu/ConversationMenu";
+import {Flex, Heading, Spacer} from "@chakra-ui/react";
+import {Conversation, getAllConversations} from "../../../services/api";
 
 type MessageLayoutProps = {};
-type MessageLayoutState = {};
+type MessageLayoutState = {conversations: Conversation[]; is_loaded: boolean, conversation_displayed: number | null};
 
 class MessageLayout extends Component<MessageLayoutProps,MessageLayoutState> {
+    constructor(props: MessageLayoutProps) {
+        super(props);
+        this.state = {conversations: [], is_loaded: false, conversation_displayed: null};
+    }
+
+    async componentDidMount() {
+        const conversation_resp = await getAllConversations();
+        this.setState({conversations: conversation_resp, is_loaded: true, conversation_displayed: null});
+    }
+
+    // Changes the conversation shown
+    updateConversationViewer = (new_conversation: number | null) => {
+        this.setState({conversation_displayed: new_conversation});
+    }
 
     render() {
-        return null
+        if(!this.state.is_loaded) {
+            return (
+                <Heading>Loading Conversations...</Heading>
+            )
+        }
+
+        return (
+            <Flex pl={4}>
+                <ConversationMenu conversations={this.state.conversations}
+                                  updateVisibleConversation={this.updateConversationViewer} />
+                <Spacer />
+            </Flex>
+        )
     }
 }
 
--- src/main/coopids-ui/src/components/ui/ConversationMenu/ConversationMenu.tsx
+++ src/main/coopids-ui/src/components/ui/ConversationMenu/ConversationMenu.tsx
@@ -1,12 +1,69 @@
 import React, {Component} from "react";
+import {
+    Box,
+    Stack,
+    Button,
+    Drawer,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerHeader,
+    DrawerBody, Heading,
+    DrawerFooter
+} from "@chakra-ui/react";
+import {Conversation} from "../../../services/api";
 
-type ConversationMenuProps = {};
-type ConversationMenuState = {};
+type ConversationMenuProps = {conversations: Conversation[]; updateVisibleConversation: (new_conversation: number | null) => void};
+type ConversationMenuState = {is_open: boolean};
 
 class ConversationMenu extends Component<ConversationMenuProps,ConversationMenuState> {
+    constructor(props: ConversationMenuProps) {
+        super(props);
+        this.state = {is_open: false};
+    }
+
+    drawerOnClick = () => {
+        this.setState({is_open: !this.state.is_open});
+    }
 
     render() {
-        return null
+        // Two options depending on size
+        return (
+                <Box float="left">
+                    {/*On regular screens, show as side bar*/}
+                    <Box display={{ base: "none", md: "block" }} pr={2} borderRight="3px solid black">
+                        <Stack spacing={8} align="center">
+                            <Heading borderBottom="3px solid black">Conversations</Heading>
+                            <p>Hello</p>
+                            <p>Hello again</p>
+                        </Stack>
+                    </Box>
+
+                    {/*When screen is too small, change side bar to draw overlay*/}
+                    <Box display={{ base: "block", md: "none" }}>
+                        <Button onClick={this.drawerOnClick}>
+                            Conversations
+                        </Button>
+                        <Drawer isOpen={this.state.is_open} onClose={()=>{}} placement="left">
+                            <DrawerOverlay>
+                                <DrawerContent>
+                                    <DrawerHeader borderBottomWidth="1px">Conversations</DrawerHeader>
+                                    <DrawerBody>
+                                        <p>Hello</p>
+                                        <p>Hello again</p>
+                                    </DrawerBody>
+                                    <DrawerFooter>
+                                        <Button onClick={this.drawerOnClick}>
+                                            Back
+                                        </Button>
+                                    </DrawerFooter>
+                                </DrawerContent>
+                            </DrawerOverlay>
+                        </Drawer>
+                    </Box>
+
+                </Box>
+            );
+
     }
 }
 
