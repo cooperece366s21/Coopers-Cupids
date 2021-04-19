58eae5061ac1221c21fff905b3220dc3a4c8b9a9
Andrew Lorber <andrewlorber@aol.com>
2021-04-19 01:58:49 -0400

    Adds Profile Page

--- src/main/coopids-ui/src/components/layouts/ProfileLayout/ProfileLayout.tsx
+++ src/main/coopids-ui/src/components/layouts/ProfileLayout/ProfileLayout.tsx
@@ -1,45 +1,53 @@
 import React, {Component} from "react";
-import { Box, Button, Flex, Stack, Image, Text } from "@chakra-ui/react";
+import {Box, Stack} from "@chakra-ui/react";
 import EditButton from "../../ui/EditButton/EditButton";
 import ProfileViewer from "../../ui/ProfileViewer/ProfileViewer";
 import {Profile, getCurrentUserProfile, setUserProfile} from "../../../services/api";
 
 type ProfileLayoutProps = {};
-type ProfileLayoutState = {is_editing: boolean; profile: Profile; editedProfile: Profile};
+type ProfileLayoutState = {is_editing: boolean; profile: Profile; has_profile: boolean};
 
 class ProfileLayout extends Component<ProfileLayoutProps,ProfileLayoutState> {
     constructor(props: ProfileLayoutProps) {
         super(props);
-        this.state = {is_editing: false, profile: null, editedProfile: null}
+        this.state = {is_editing: false, has_profile: false, profile: {} as Profile};
     }
 
-    componentDidMount() {
-        getCurrentUserProfile().then(json => this.setState({profile: json, editedProfile: json}))
-    }
+    async componentDidMount() {
+        const json = await getCurrentUserProfile();
 
-    onEditButtonClick = async () => {
-        // Checks if need to save
-        if(this.state.is_editing) {
-            // Make API call
-            const newProfile = await setUserProfile(this.state.editedProfile)
-                .then(json => this.setState({profile: json, editedProfile: json}));
+        if(json != null) {
+            this.setState({is_editing: false, profile: json, has_profile: true})
         }
-        this.setState({is_editing: !this.state.is_editing})
     }
 
-    updateProfile = (key: string, value: string | number | null) => {
-        this.setState(prevState => ({editedProfile: {...prevState.editedProfile, key: value}}));
+    onEditButtonClick = () => {
+        this.setState({is_editing: true});
+    }
+
+    updateProfile = async (new_profile: Profile) => {
+        const json = await setUserProfile(new_profile);
+        // Should never be null if we're updating the profile
+        // Only checking to make TypeScript happy
+        if(json != null) {
+            this.setState({profile: json, has_profile: true, is_editing: false});
+        }
     }
 
     render() {
         return (
-            <Flex width="full" align="center" justifyContent="center">
+            <Stack width="full" align="center" justifyContent="center" spacing={4}>
+                <ProfileViewer is_editing={this.state.is_editing} profile={this.state.profile}
+                               has_profile={this.state.has_profile} editProfile={this.updateProfile}/>
                 {/* Edit Button */}
-                <Box float="right">
-                    <EditButton is_editing={this.state.is_editing} onClick={this.onEditButtonClick}/>
-                </Box>
-                <ProfileViewer is_editing={this.state.is_editing} profile={this.state.editedProfile}/>
-            </Flex>
+                {/*This button is separate, so the ProfileViewer Component can be reused on the feed page*/}
+                {!this.state.is_editing ?
+                    <Box>
+                        <EditButton has_profile={this.state.has_profile} onClick={this.onEditButtonClick}/>
+                    </Box>
+                    : null
+                }
+            </Stack>
         )
     }
 }
