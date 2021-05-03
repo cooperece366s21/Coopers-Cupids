9de5996c7e838039631b50cb6361b3aa2afb05d1
Andrew Lorber <andrewlorber@aol.com>
2021-05-03 00:58:58 -0400

    Fixes navbar margins and mobile view

--- src/main/coopids-ui/src/components/sections/Navbar/Navbar.tsx
+++ src/main/coopids-ui/src/components/sections/Navbar/Navbar.tsx
@@ -21,13 +21,18 @@ class NavBar extends Component<NavBarProps, NavBarState> {
 
     render() {
         return (
-            <Grid as="nav" w="fill" mb={8} p={8} templateColumns="repeat(2,auto)" borderBottom={"solid #cfcdcc .5px"}>
+            <Grid as="nav" w="fill" p={6} templateColumns="repeat(2,auto)" borderBottom={"solid #cfcdcc .5px"}>
+                {/* Logo */}
                 <GridItem display="inline" justifySelf="flex-start" pl={4}>
                     <NavBarLink linkTo={"/"} onPageChange={this.closeNavBar}>
                         <Image src="images/Coopids_logo_large.png" width="9em"/>
                     </NavBarLink>
                 </GridItem>
-                <NavBarToggleButton onToggle={this.toggleNavBar} isOpen={this.state.isOpen} />
+                {/* Navbar Toggle Button - On small screens*/}
+                {this.props.isLoggedIn ?
+                    <NavBarToggleButton onToggle={this.toggleNavBar} isOpen={this.state.isOpen} />
+                    : null }
+                {/* Navbar Links*/}
                 <GridItem display={{ base: this.state.isOpen ? "block" : "none", md: "block" }} colSpan={[2,2,1,1]}>
                     <Stack spacing={[4,4,10,20]} align="center" justify={["center", "center", "flex-end", "flex-end"]}
                         direction={["column", "column", "row", "row"]} pt={5} pr={4}>
