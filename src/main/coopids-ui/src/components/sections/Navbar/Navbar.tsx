6029a62860a168dbc91bd0b659aa9c3c2e8900a1
Andrew Lorber <andrewlorber@aol.com>
2021-05-03 00:25:31 -0400

    Adds border & centers Navbar

--- src/main/coopids-ui/src/components/sections/Navbar/Navbar.tsx
+++ src/main/coopids-ui/src/components/sections/Navbar/Navbar.tsx
@@ -19,20 +19,18 @@ class NavBar extends Component<NavBarProps, NavBarState> {
     // Closes Navbar on page change
     closeNavBar = () => {this.setState({isOpen: false});}
 
-    // TODO: Only show home page when not logged in
     render() {
         return (
-            <Grid as="nav" w="fill" mb={8} p={8} templateColumns="repeat(2,auto)">
+            <Grid as="nav" w="fill" mb={8} p={8} templateColumns="repeat(2,auto)" borderBottom={"solid #cfcdcc .5px"}>
                 <GridItem display="inline" justifySelf="flex-start" pl={4}>
                     <NavBarLink linkTo={"/"} onPageChange={this.closeNavBar}>
                         <Image src="images/Coopids_logo_large.png" width="9em"/>
                     </NavBarLink>
                 </GridItem>
                 <NavBarToggleButton onToggle={this.toggleNavBar} isOpen={this.state.isOpen} />
-                <GridItem display={{ base: this.state.isOpen ? "block" : "none", md: "block" }}>
+                <GridItem display={{ base: this.state.isOpen ? "block" : "none", md: "block" }} colSpan={[2,2,1,1]}>
                     <Stack spacing={[4,4,10,20]} align="center" justify={["center", "center", "flex-end", "flex-end"]}
                         direction={["column", "column", "row", "row"]} pt={5} pr={4}>
-                        {/*<NavBarLink linkTo={"/"} onPageChange={this.closeNavBar}>Home</NavBarLink>*/}
                         {this.props.isLoggedIn ?
                             <NavBarLink linkTo={"/Profile"} onPageChange={this.closeNavBar}>Profile</NavBarLink>
                             : null }
