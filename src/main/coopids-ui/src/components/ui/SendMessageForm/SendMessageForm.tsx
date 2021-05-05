1f53dbc4fa434232692ee55a7c7cf358ab96da7a
Andrew Lorber <andrewlorber@aol.com>
2021-05-05 19:43:12 -0400

    Fixes styling of message sending fields

--- src/main/coopids-ui/src/components/ui/SendMessageForm/SendMessageForm.tsx
+++ src/main/coopids-ui/src/components/ui/SendMessageForm/SendMessageForm.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Button, Flex, FormControl, Input} from "@chakra-ui/react";
+import {Box, Button, Flex, FormControl, Input} from "@chakra-ui/react";
 
 type SendMessageFormProps = {sendMessage: (toUserID: string, newMessage: string) => void;
                              toUserID: string};
@@ -17,15 +17,19 @@ class SendMessageForm extends Component<SendMessageFormProps,SendMessageFormStat
                             this.props.sendMessage(this.props.toUserID,  this.state.newMessage);
                             this.setState({newMessage: ""});}}
             >
-                <Flex>
-                    <FormControl isRequired float="left">
+                <Flex mr={6} ml={6}>
+                    <FormControl isRequired>
                         <Input type="text" value={this.state.newMessage} aria-label="Message-Input"
+                               placeholder={"Write a message to your match!"}
+                               _placeholder={{"font-size": ".9em", "align-self": "center"}}
                                onChange={e => this.setState({newMessage: e.currentTarget.value})}
+                               borderColor="#FFFFFF"
                         />
                     </FormControl>
                     {/* Send Message Button */}
-                    <Button width="full" float="right" boxShadow='sm' _hover={{boxShadow: 'md'}}
-                            _active={{boxShadow: 'lg'}} type="submit" color="green">
+                    <Button type="submit" width="20%" ml={4} justifySelf="flex-end" boxShadow='sm'
+                            color="green" backgroundColor={"#FFFFFF"} _hover={{boxShadow: 'md',
+                            backgroundColor: "#F2BBC1", border: "1px solid green"}} _active={{boxShadow: 'lg'}}>
                         Send
                     </Button>
                 </Flex>
